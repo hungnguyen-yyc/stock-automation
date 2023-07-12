493e8e4b2ef69c5f382a9bca9d1e9d4bc591229b
Hung Nguyen <hung.nguyen@magnetforensics.com>
2023-07-12 04:31:48 -0600

    bug fix and clean up

--- day-trading-signals/Program.cs
+++ day-trading-signals/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Skender.Stock.Indicators;
 using StockSignalScanner.Models;
+using Xunit.Sdk;
 
 public class Program
 {
@@ -9,7 +10,7 @@ public class Program
 
     public static async Task Main(string[] args)
     {
-        var favs = new List<string>() { "AMD", "AAPL", "GOOGL", "TSLA", "NVDA", "META", "AMZN", "COIN", "MARA", "RIOT" };
+        var favs = new List<string>() { "AMD", "AAPL", "GOOGL", "TSLA", "NVDA", "META", "AMZN", "COIN", "MARA", "RIOT", "RBLX", "SPY" };
         using (var httpClient = new HttpClient())
         {
             foreach (var ticker in favs)
@@ -27,10 +28,11 @@ public class Program
                     {
                         return;
                     }
-                    var result = DetectDivergence(ticker, tickerHistoricalPrices.Reverse().ToList());
+                    var result = DetectDivergenceByTopAndBottomMfis(ticker, tickerHistoricalPrices.Reverse().ToList());
 
                     if (result.Any())
                     {
+                        result = result.Reverse().ToList();
                         var todayPath = $@"{PATH}\{DateTime.Now.ToString("yyyy-MM-dd")}";
                         if (Directory.Exists(PATH) == false)
                         {
@@ -42,24 +44,26 @@ public class Program
                         }
                         File.WriteAllText($@"{todayPath}\{ticker}.txt", string.Join("\n", result));
                     }
-
-                    Console.WriteLine("\n");
-                    Console.WriteLine("\n");
                 }
             }
         }
     }
 
-    public static IList<string> DetectDivergence(string ticker, IList<Price> prices)
+    public static IList<string> DetectDivergenceByTopAndBottomMfis(string ticker, IList<Price> prices)
     {
         var result = new List<string>();
         var timeBetweenPivots = 60;
-        var numberOf5MinIn2TradingDays = 79 * 2;
+        var numberOf5MinInLast1TradingDays = 79;
+        var limitHighestMfi = 30;
         var mfis = prices.GetMfi(14).ToList();
-        var mfisLast2Days = mfis.Skip(mfis.Count - numberOf5MinIn2TradingDays).Take(numberOf5MinIn2TradingDays).ToList();
+        var macds = prices.GetMacd(12, 26, 9).ToList();
+        var mfisLast2Days = mfis.Skip(mfis.Count - numberOf5MinInLast1TradingDays).Take(numberOf5MinInLast1TradingDays).ToList();
 
-        var highestMfisOrderByDate = mfisLast2Days.OrderByDescending(m => m.Mfi).Take(20).OrderBy(m => m.Date).ToList();
+        var highestMfisOrderByDate = mfisLast2Days.OrderByDescending(m => m.Mfi).Take(limitHighestMfi).OrderBy(m => m.Date).ToList();
         var pricesAtMfiDate = prices.Where(p => highestMfisOrderByDate.Any(m => m.Date == p.Date)).OrderBy(m => m.Date).ToList();
+        var macdsAtMfiDate = macds.Where(p => highestMfisOrderByDate.Any(m => m.Date == p.Date)).OrderBy(m => m.Date).ToList();
+
+        var startDateMap = new Dictionary<DateTime, string>();
 
         for (int i = 0; i < highestMfisOrderByDate.Count; i++)
         {
@@ -85,28 +89,57 @@ public class Program
                 } 
                 else
                 {
-                    goodToCheck = true;
+                    if (price1Hour == 15 || price2Hour == 15)
+                    {
+                        goodToCheck = false;
+                    }
+                    else
+                    {
+                        goodToCheck = true;
+                    }
                 }
 
                 if (goodToCheck)
                 {
-                    if (mfiResult1.Mfi > mfiResult2.Mfi && price1.High < price2.High)
+                    var macd = macdsAtMfiDate[j];
+                    var macdString = "";
+                    var temaString = "";
+                    var resultString = "";
+                    if (macd.Macd < macd.Signal)
                     {
-                        result.Add($"- {ticker}: Regular Bearish Divergence Found for at {price1.Date} and {price2.Date}");
-                        Console.WriteLine($"- {ticker}: Regular Bearish Divergence Found for at {price1.Date} and {price2.Date}");
+                        macdString = $"*** MACD ({macd.Macd.Value.ToString("0.##")}) < Signal ({macd.Signal.Value.ToString("0.##")}) ***";
                     }
-                    if (mfiResult1.Mfi < mfiResult2.Mfi && price1.High > price2.High)
+                    if (price2.Close <= price2.Tema)
                     {
-                        result.Add($"- {ticker}: Hidden Bearish Divergence Found for at {price1.Date} and {price2.Date}");
-                        Console.WriteLine($"- {ticker}: Hidden Bearish Divergence Found for at {price1.Date} and {price2.Date}");
+                        temaString = $"*** Close ({price2.Close}) <= Tema ({price2.Tema}) ***";
+                    }
+
+                    if (mfiResult1.Mfi > mfiResult2.Mfi && price1.High <= price2.High)
+                    {
+                        resultString = $"- {ticker}: Regular Bearish Divergence Found for at {price1.Date} and {price2.Date} | {macdString} | {temaString}";
+                    }
+                    if (mfiResult1.Mfi < mfiResult2.Mfi && price1.High >= price2.High)
+                    {
+                        resultString = $"- {ticker}: Hidden Bearish Divergence Found for at {price1.Date} and {price2.Date} | {macdString} | {temaString}";
+                    }
+
+                    if (!string.IsNullOrEmpty(resultString))
+                    {
+                        if (!startDateMap.ContainsKey(price1.Date))
+                        {
+                            startDateMap.Add(price1.Date, resultString);
+                        }
+                        else
+                        {
+                            startDateMap[price1.Date] = resultString;
+                        }
                     }
                 }
 
             }
         }
 
-
-        var lowestMfisOrderByDate = mfisLast2Days.OrderByDescending(m => m.Mfi).Take(20).OrderBy(m => m.Date).ToList();
+        var lowestMfisOrderByDate = mfisLast2Days.OrderBy(m => m.Mfi).Take(limitHighestMfi).OrderBy(m => m.Date).ToList();
         pricesAtMfiDate = prices.Where(p => lowestMfisOrderByDate.Any(m => m.Date == p.Date)).OrderBy(m => m.Date).ToList();
 
         for (int i = 0; i < lowestMfisOrderByDate.Count; i++)
@@ -119,13 +152,14 @@ public class Program
 
             for (int j = i + 1; j < lowestMfisOrderByDate.Count; j++)
             {
-                var mfiResult2 = lowestMfisOrderByDate[i];
-                var price2 = pricesAtMfiDate[i];
+                var mfiResult2 = lowestMfisOrderByDate[j];
+                var price2 = pricesAtMfiDate[j];
                 var price2Day = price2.Date.Day;
                 var price2Hour = price2.Date.Hour;
                 var price2Minute = price2.Date.Minute;
 
                 var goodToCheck = false;
+
                 if (price1Day == price2Day)
                 {
                     var timeDiff = price1Hour * 60 + price1Minute - (price2Hour * 60 + price2Minute);
@@ -133,24 +167,59 @@ public class Program
                 }
                 else
                 {
-                    goodToCheck = true;
+                    if (price1Hour == 15 || price2Hour == 15)
+                    {
+                        goodToCheck = false;
+                    }
+                    else
+                    {
+                        goodToCheck = true;
+                    }
                 }
 
                 if (goodToCheck)
                 {
-                    if (mfiResult1.Mfi < mfiResult2.Mfi && price1.Low > price2.Low)
+                    var macd = macdsAtMfiDate[j];
+                    var macdString = "";
+                    var temaString = "";
+                    var resultString = "";
+                    if (macd.Macd > macd.Signal)
                     {
-                        result.Add($"- {ticker}: Regular Bullish Divergence Found for at {price1.Date} and {price2.Date}");
-                        Console.WriteLine($"- {ticker}: Regular Bullish Divergence Found for at {price1.Date} and {price2.Date}");
+                        macdString = $"*** MACD ({macd.Macd.Value.ToString("0.##")}) > Signal ({macd.Signal.Value.ToString("0.##")}) ***";
                     }
-                    if (mfiResult1.Mfi > mfiResult2.Mfi && price1.Low < price2.Low)
+                    if (price2.Close >= price2.Tema)
                     {
-                        result.Add($"- {ticker}: Hidden Bullish Divergence Found for at {price1.Date} and {price2.Date}");
-                        Console.WriteLine($"- {ticker}: Hidden Bullish Divergence Found for at {price1.Date} and {price2.Date}");
+                        temaString = $"*** Close ({price2.Close}) >= Tema ({price2.Tema}) ***";
+                    }
+
+                    if (mfiResult1.Mfi < mfiResult2.Mfi && price1.Low >= price2.Low)
+                    {
+                        resultString = $"- {ticker}: Regular Bullish Divergence Found for at {price1.Date} and {price2.Date} | {macdString} | {temaString}";
+                    }
+                    if (mfiResult1.Mfi > mfiResult2.Mfi && price1.Low <= price2.Low)
+                    {
+                        resultString = $"- {ticker}: Hidden Bullish Divergence Found for at {price1.Date} and {price2.Date} | {macdString} | {temaString}";
+                    }
+
+                    if (!string.IsNullOrEmpty(resultString))
+                    {
+                        if (!startDateMap.ContainsKey(price1.Date))
+                        {
+                            startDateMap.Add(price1.Date, resultString);
+                        }
+                        else
+                        {
+                            startDateMap[price1.Date] = resultString;
+                        }
                     }
                 }
 
             }
+        }
+
+        foreach (var item in startDateMap)
+        {
+            result.Add(item.Value);
         }
         return result;
     }
