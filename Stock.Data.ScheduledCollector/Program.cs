c5bc63e3023dceb5a6ab921c9fc9f6f85c4172fc
Hung Nguyen <hungnguyenyyc@gmail.com>
2024-07-01 11:51:39 -0600

    update collector params

--- Stock.Data.ScheduledCollector/Program.cs
+++ Stock.Data.ScheduledCollector/Program.cs
@@ -24,13 +24,13 @@ namespace Stock.Data.ScheduledCollector
             foreach (var ticker in tickers)
             {
                 Log($"Collecting data for {ticker} at {DateTime.Now:s}");
-                await dbHandler.QuickFill(ticker, Timeframe.Minute15, DateTime.Today.AddDays(-20));
-                await dbHandler.QuickFill(ticker, Timeframe.Minute30, DateTime.Today.AddDays(-20));
-                await dbHandler.QuickFill(ticker, Timeframe.Hour1, DateTime.Today.AddDays(-20));
-                await dbHandler.QuickFill(ticker, Timeframe.Daily, DateTime.Today.AddDays(-20));
+                await dbHandler.QuickFill(ticker, Timeframe.Minute15, DateTime.Today.AddDays(-60));
+                await dbHandler.QuickFill(ticker, Timeframe.Minute30, DateTime.Today.AddDays(-60));
+                await dbHandler.QuickFill(ticker, Timeframe.Hour1, DateTime.Today.AddDays(-60));
+                await dbHandler.QuickFill(ticker, Timeframe.Daily, DateTime.Today.AddDays(-60));
                 Log($"Finished collecting data for {ticker} at {DateTime.Now:s}");
                 Log("+++++++++++++++++++++++++++++++++++++++++");
-                await Task.Delay(TimeSpan.FromSeconds(15));
+                await Task.Delay(TimeSpan.FromSeconds(5));
             }
 
             Log("Finished collecting data for all tickers. Waiting for next trigger.");
--- Stock.Shared/TickersToTrade.cs
+++ Stock.Shared/TickersToTrade.cs
@@ -5,7 +5,7 @@
 #if !DEBUG
         public static readonly List<string> POPULAR_TICKERS = new List<string> { "AMD", "MSFT", "RIVN", "AAPL", "GOOGL", "TSLA", "NVDA", "META", "AMZN", "COIN", "RBLX", "SPY", "QQQ", "CAT", "DIS", "V", "SCHW", "MARA", "RIOT", "CVNA", "PLTR", "NIO", "AAL", "TD" };
 #else
-        public static readonly List<string> POPULAR_TICKERS = new List<string> { "RIVN", "AAPL", "GOOGL", "TSLA", "NVDA", "META", "AMZN", "COIN", "RBLX", "SPY", "QQQ", "CAT", "DIS", "V", "SCHW", "MARA", "RIOT", "CVNA", "PLTR", "NIO", "AAL", "TD" };
+        public static readonly List<string> POPULAR_TICKERS = new List<string> { "AMD", "MSFT", "RIVN", "AAPL", "GOOGL", "TSLA", "NVDA", "META", "AMZN", "COIN", "RBLX", "SPY", "QQQ", "CAT", "DIS", "V", "SCHW", "MARA", "RIOT", "CVNA", "PLTR", "NIO", "AAL", "TD" };
 #endif
     }
 }
