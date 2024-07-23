e5e5614dce2e50c193e5bd2e9da49615b0857f4e
hungnguyen-yyc <81936797+hungnguyen-yyc@users.noreply.github.com>
2024-07-22 22:24:29 -0600

    Remove ibkr (#2)
    * remove IBKR from UI
    
    * remove more IBKR

--- Stock.UI/Components/MainScreenViewModel.cs
+++ Stock.UI/Components/MainScreenViewModel.cs
@@ -5,32 +5,21 @@ using Stock.Shared;
 using Stock.Shared.Models;
 using Stock.Shared.Models.IBKR.Messages;
 using Stock.Strategies;
-using Stock.UI.IBKR.Client;
-using Stock.UI.IBKR.Managers;
-using Stock.UI.IBKR.Messages;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Windows.Data;
 using Microsoft.Toolkit.Uwp.Notifications;
 using Stock.Strategies.EventArgs;
-using Contract = IBApi.Contract;
-using Order = IBApi.Order;
 
 namespace Stock.UI.Components
 {
     public class MainScreenViewModel : INotifyPropertyChanged
     {
         private const string ALL = "All";
-        private const string ClientId = "1"; // hardcoded for now, may not needed
-        private string _accountId = string.Empty;
-
-        private IBClient _ibClient;
-        private EReaderMonitorSignal _signal;
 
         private readonly StockDataRepository _repo;
         private ISwingPointStrategy _strategy;
-        private IReadOnlyCollection<Price> _prices;
         private string _selectedTimeframe;
         private string _selectedTicker;
         private string _selectedOptionType;
@@ -40,28 +29,20 @@ namespace Stock.UI.Components
         private ObservableCollection<string> _filteredOptionChain;
         private ObservableCollection<string> _optionPrice;
         private readonly object _lock = new();
-        private AccountManager _accountManager;
-        private PnLManager _pnlManager;
-        private OrderManager _orderManager;
         private ObservableCollection<CompletedOrderMessage> _completedOrders;
         private ObservableCollection<Tuple<string, string>> _accountSummary;
         private ObservableCollection<PositionMessage> _accountPosition;
         private ObservableCollection<TrendLine> _allTrendLines;
         private ObservableCollection<TrendLine> _filteredTrendLines;
-        private string _pnL;
         private SwingPointPositionTrackingService _swingPointPositionTrackingService;
 
         private Dictionary<string, IReadOnlyCollection<Price>> _tickerAndPrices;
         private IReadOnlyCollection<string> _allOptionTypes;
 
-        public delegate void IBKRConnectedHandler(bool isConnected);
-        public event IBKRConnectedHandler IBKRConnected;
-
         public MainScreenViewModel(StockDataRepository repo, SwingPointsLiveTradingLowTimeframesStrategy strategy)
         {
             _repo = repo;
             _strategy = strategy;
-            _prices = new List<Price>();
             _selectedTimeframe = ALL;
             _selectedTicker = ALL;
             _selectedOptionType = ALL;
@@ -83,29 +64,16 @@ namespace Stock.UI.Components
             BindingOperations.EnableCollectionSynchronization(_filteredOptionChain, _lock);
             BindingOperations.EnableCollectionSynchronization(_optionPrice, _lock);
 
-            _signal = new EReaderMonitorSignal();
-            _ibClient = new IBClient(_signal);
-            _ibClient.NextValidId += OnNextValidId;
-            _accountManager = new AccountManager(_ibClient);
             _accountSummary = new ObservableCollection<Tuple<string, string>>();
             _accountPosition = new ObservableCollection<PositionMessage>();
-            _accountManager.AccountSummaryReceived += OnAccountSummaryReceived;
-            _accountManager.PositionReceived += OnPositionReceived;
 
             BindingOperations.EnableCollectionSynchronization(_accountSummary, _lock);
             BindingOperations.EnableCollectionSynchronization(_accountPosition, _lock);
-
-            _pnlManager = new PnLManager(_ibClient);
-            _pnlManager.PnLReceived += OnPnLReceived;
-
-            _orderManager = new OrderManager(_ibClient);
-            _orderManager.CompletedOrderReceived += OnCompletedOrderReceived;
 
             _completedOrders = new ObservableCollection<CompletedOrderMessage>();
             BindingOperations.EnableCollectionSynchronization(_completedOrders, _lock);
 
             _swingPointPositionTrackingService = new SwingPointPositionTrackingService(_repo);
-            _swingPointPositionTrackingService.ClosePositionAlert += HandleClosePosition;
 
             _repo.LogCreated += (message) =>
             {
@@ -153,98 +121,6 @@ namespace Stock.UI.Components
             }
         }
 
-        public string PnL { 
-            get => _pnL;
-            private set
-            {
-                _pnL = value;
-                OnPropertyChanged(nameof(PnL));
-            }
-        }
-
-        private void OnPnLReceived(int reqId, double dailyPnL, double unrealizedPnL, double realizedPnL)
-        {
-            Logs.Add(new LogEventArg("PnL received."));
-            PnL = $"Daily PnL: {dailyPnL}\nUnrealized PnL: {unrealizedPnL}\nRealized PnL: {realizedPnL}";
-        }
-
-        private void OnPositionReceived(PositionMessage positionMessage)
-        {
-            Logs.Add(new LogEventArg("Position received."));
-            var existedPosition = _accountPosition.FirstOrDefault(x => 
-                x.Account == positionMessage.Account 
-                && x.Contract.Symbol == positionMessage.Contract.Symbol
-                && x.Contract.Currency == positionMessage.Contract.Currency
-                && x.Contract.SecType == positionMessage.Contract.SecType
-                && x.Contract.Exchange == positionMessage.Contract.Exchange
-                && x.Contract.LastTradeDateOrContractMonth == positionMessage.Contract.LastTradeDateOrContractMonth);
-
-            if (existedPosition != null)
-            {
-                _accountPosition.Remove(existedPosition);
-            }
-
-            _accountPosition.Add(positionMessage);
-            _swingPointPositionTrackingService.UpdatePosition(positionMessage).Wait();
-        }
-
-        private string AccountId
-        {
-            get => _accountId;
-            set
-            {
-                if (_accountId != value)
-                {
-                    _accountId = value;
-                    _pnlManager.ReqPnL(_accountId, "");
-                    OnPropertyChanged(nameof(AccountId));
-                }
-            }
-        }
-
-        private void OnAccountSummaryReceived(AccountSummaryMessage accountSummary)
-        {
-            Logs.Add(new LogEventArg("Account summary received."));
-            var ACCOUNT = "Account";
-            var CURRENCY = "Currency";
-            var hasAccount = AccountSummary.Any(x => x.Item1 == ACCOUNT);
-            if (!hasAccount)
-            {
-                AccountSummary.Add(new Tuple<string, string>(ACCOUNT, accountSummary.Account));
-                AccountId = accountSummary.Account;
-            }
-            else
-            {
-                var account = AccountSummary.First(x => x.Item1 == ACCOUNT);
-                AccountSummary.Remove(account);
-                AccountSummary.Insert(0, new Tuple<string, string>(ACCOUNT, accountSummary.Account));
-            }
-
-            var hasCurrency = AccountSummary.Any(x => x.Item1 == CURRENCY);
-            if (!hasCurrency)
-            {
-                AccountSummary.Add(new Tuple<string, string>(CURRENCY, accountSummary.Currency));
-            }
-            else
-            {
-                var currency = AccountSummary.First(x => x.Item1 == CURRENCY);
-                AccountSummary.Remove(currency);
-                AccountSummary.Insert(1, new Tuple<string, string>(CURRENCY, accountSummary.Currency));
-            }
-
-            var hasTag = AccountSummary.Any(x => x.Item1 == accountSummary.Tag);
-            if (!hasTag)
-            {
-                AccountSummary.Add(new Tuple<string, string>(accountSummary.Tag, accountSummary.Value));
-            }
-            else
-            {
-                var account = AccountSummary.First(x => x.Item1 == accountSummary.Tag);
-                AccountSummary.Remove(account);
-                AccountSummary.Add(new Tuple<string, string>(accountSummary.Tag, accountSummary.Value));
-            }
-        }
-
         public bool IsConnected { get; private set; }
         public ObservableCollection<Tuple<string, string>> AccountSummary { 
             get => _accountSummary; 
@@ -263,80 +139,6 @@ namespace Stock.UI.Components
                 _accountPosition = value;
                 OnPropertyChanged(nameof(Positions));
             }
-        }
-
-        public void Connect(string host = "127.0.0.1", int port = 7496)
-        {
-            Logs.Add(new LogEventArg("Connecting to IBKR..."));
-            if (!IsConnected)
-            {
-                if (host == null || host.Equals(""))
-                    host = "127.0.0.1";
-                try
-                {
-                    _ibClient.ClientId = int.Parse(ClientId);
-                    _ibClient.ClientSocket.eConnect(host, port, _ibClient.ClientId);
-
-                    var reader = new EReader(_ibClient.ClientSocket, _signal);
-
-                    reader.Start();
-
-                    new Thread(() => { while (_ibClient.ClientSocket.IsConnected()) { _signal.waitForSignal(); reader.processMsgs(); } }) { IsBackground = true }.Start();
-                }
-                catch (Exception)
-                {
-                    Logs.Add(new LogEventArg("Error code -1. Please check your connection attributes."));
-                }
-            }
-            else
-            {
-                Disconnect();
-            }
-        }
-
-        public void Disconnect()
-        {
-            if (IsConnected)
-            {
-                IsConnected = false;
-                _ibClient.ClientSocket.eDisconnect();
-                AccountSummary.Clear();
-                Positions.Clear();
-                _completedOrders.Clear();
-                PnL = "PnL: n/a";
-                IBKRConnected?.Invoke(IsConnected);
-                Logs.Add(new LogEventArg("Disconnected from IBKR."));
-            }
-        }
-
-        private void OnNextValidId(ConnectionStatusMessage statusMessage)
-        {
-            Logs.Add(new LogEventArg($"Connection status: {statusMessage.IsConnected}"));
-            IsConnected = statusMessage.IsConnected;
-            IBKRConnected?.Invoke(IsConnected);
-            GetAccountSummary();
-            GetCompletedOrders();
-        }
-
-        private void GetAccountSummary()
-        {
-            Logs.Add(new LogEventArg("Requesting account summary..."));
-            _accountManager.RequestAccountSummary();
-            _accountManager.RequestPositions();
-            Logs.Add(new LogEventArg("Requesting account summary completed."));
-        }
-
-        private void GetCompletedOrders()
-        {
-            Task.Run(async () => { 
-                while (IsConnected)
-                {
-                    Logs.Add(new LogEventArg("Requesting completed orders..."));
-                    _ibClient.ClientSocket.reqCompletedOrders(false);
-                    Logs.Add(new LogEventArg("Requesting completed orders completed."));
-                    await Task.Delay(TimeSpan.FromMinutes(15));
-                }
-            });
         }
 
         public IReadOnlyCollection<string> AllOptionTypes => _allOptionTypes;
@@ -815,108 +617,8 @@ namespace Stock.UI.Components
                 }
                     
                 _allAlerts.Add(alert);
-                CreateTopNBottomBuyOrder(alert).Wait();
                 UpdateFilteredAlerts();
             }
-        }
-
-        private async Task CreateTopNBottomBuyOrder(Alert alert)
-        {
-            if (alert is not TopNBottomStrategyAlert)
-            {
-                return;
-            }
-
-            var swingPointAlert = (TopNBottomStrategyAlert)alert;
-            var atr = swingPointAlert.ATR;
-            var today = DateTime.Now.Date;
-            int numberOfDaysToFriday = (int)DayOfWeek.Friday - (int)today.DayOfWeek;
-            var fridayThisWeek = DateTime.Now.AddDays(numberOfDaysToFriday);
-
-            var fridayNextWeek = today.AddDays(numberOfDaysToFriday + 7);
-            var availableContracts = await _repo.GetOptionChainAsync(swingPointAlert.Ticker, today, fridayNextWeek);
-
-            if (availableContracts == null || !availableContracts.Active.Any())
-            {
-                return;
-            }
-
-            var optionType = alert.OrderPosition == OrderPosition.Long ? OptionType.C.ToString() : OptionType.P.ToString();
-            var strike = alert.OrderPosition == OrderPosition.Long ? swingPointAlert.PriceClosed + atr * 3 : swingPointAlert.PriceClosed - atr * 3;
-            Option closestOption = null;
-            List<Option> optionsToPick = null;
-            if (numberOfDaysToFriday >= 3)
-            {
-                optionsToPick = availableContracts.ParsedActiveOptions.Where(x => x.ExpiryDate == fridayThisWeek).ToList();
-            }
-            else
-            {
-                optionsToPick = availableContracts.ParsedActiveOptions.Where(x => x.ExpiryDate == fridayNextWeek).ToList();
-            }
-
-            if (!optionsToPick.Any())
-            {
-                return;
-            }
-
-            var option = optionsToPick.OrderBy(opt => Math.Abs(opt.StrikePrice - strike)).First();
-
-            var order = new Order
-            {
-                OrderId = 0,
-                Action = OrderAction.BUY.ToString(),
-                OrderType = OrderType.MKT.ToString(),
-                TotalQuantity = 10,
-                Transmit = true,
-                Account = _accountId,
-            };
-            var contract = new Contract
-            {
-                Symbol = swingPointAlert.Ticker,
-                SecType = SecType.OPT.ToString(),
-                Currency = Currency.USD.ToString(),
-                Exchange = "SMART",
-                Strike = (double)option.StrikePrice,
-                Right = optionType,
-                Multiplier = "100",
-                LastTradeDateOrContractMonth = option.ExpiryDate.ToString("yyyyMMdd"),
-            };
-
-#if !DEBUG
-            if (!IsConnected)
-            {
-                return;
-            }
-            _orderManager.PlaceOrder(contract, order);
-            await _repo.SaveContract(contract);
-            await _repo.SaveOptionContractWithTarget(contract, (TopNBottomStrategyAlert)alert);
-#endif
-            Logs.Add(new LogEventArg($"Order placed for {swingPointAlert.Ticker} {optionType} {option.StrikePrice} {option.ExpiryDate}"));
-        }
-
-        private void HandleClosePosition(PositionMessage positionMessage)
-        {
-            var order = new Order
-            {
-                OrderId = 0,
-                Action = OrderAction.SELL.ToString(),
-                OrderType = OrderType.MKT.ToString(),
-                TotalQuantity = positionMessage.Position,
-                Transmit = true,
-                Account = _accountId,
-            };
-            var contract = positionMessage.Contract;
-
-#if !DEBUG
-            if (!IsConnected)
-            {
-                return;
-            }
-            _orderManager.PlaceOrder(contract, order);
-            _repo.SaveContract(contract);
-#else
-            Logs.Add(new LogEventArg($"Closed position for {positionMessage.Contract.Symbol} {positionMessage.Contract.Right} {positionMessage.Contract.Strike} {positionMessage.Contract.LastTradeDateOrContractMonth}"));
-#endif
         }
 
         public void ExportAlertToCsv()
