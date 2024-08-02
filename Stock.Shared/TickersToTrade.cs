8efc126405e72ab257b22a7d43f0a4867e52577f
Hung Nguyen <hung.nguyen@magnetforensics.com>
2024-08-01 22:02:52 -0600

    canadian stocks

--- Stock.Shared/TickersToTrade.cs
+++ Stock.Shared/TickersToTrade.cs
@@ -3,7 +3,7 @@
     public class TickersToTrade
     {
 #if !DEBUG
-        public static readonly List<string> POPULAR_TICKERS = new List<string> { "AMD", "MSFT", "RIVN", "AAPL", "GOOGL", "TSLA", "NVDA", "META", "AMZN", "COIN", "RBLX", "SPY", "QQQ", "CAT", "DIS", "V", "SCHW", "MARA", "RIOT", "CVNA", "PLTR", "NIO", "AAL", "TD", "XOM", "CHWY", "SBUX" };
+        public static readonly List<string> POPULAR_TICKERS = new List<string> { "ENB.TO", "SU.TO", "BNS.TO", "TD.TO", "RY.TO", "SHOP.TO", "BMO.TO", "BITF.TO", "CNQ.TO", "AC.TO", "T.TO", "MFC.TO", "SLF.TO"};
 #else
         public static readonly List<string> POPULAR_TICKERS = new List<string> { "AMD", "MSFT", "RIVN", "AAPL", "GOOGL", "TSLA", "NVDA", "META", "AMZN", "COIN", "RBLX", "SPY", "QQQ", "CAT", "DIS", "V", "SCHW", "MARA", "RIOT", "CVNA", "PLTR", "NIO", "AAL", "TD", "XOM", "CHWY", "SBUX" };
 #endif
