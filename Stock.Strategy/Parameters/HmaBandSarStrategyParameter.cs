16f11f29cbddf8f117c2520e7bea603030a13f34
Hung Nguyen <hungnguyenyyc@gmail.com>
2024-10-16 00:47:40 -0600

    add hma ema price strategy

--- Stock.Strategy/Parameters/HmaBandSarStrategyParameter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Stock.Shared.Models.Parameters;
-
-namespace Stock.Strategies.Parameters
-{
-    public class HmaBandSarStrategyParameter : IStrategyParameter
-    {
-        SarParameter Sar { get; set; }
-    }
-}
--- Stock.Strategy/Parameters/IStrategyParameter.cs
+++ Stock.Strategy/Parameters/IStrategyParameter.cs
@@ -1,6 +1,9 @@
-﻿namespace Stock.Strategies.Parameters
+﻿using Stock.Shared.Models;
+
+namespace Stock.Strategies.Parameters
 {
     public interface IStrategyParameter
     {
+        public Timeframe Timeframe { get; set; }
     }
 }
